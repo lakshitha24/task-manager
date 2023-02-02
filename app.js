47f0864ae87774c3d4a89b0f5aba8408edd6b04a
Lakshitha <lakshitha.mihiran@wundermanthompson.com>
2023-02-02 12:46:55 +0530

    env changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const connectDB = require('./db/connection');
 require('dotenv').config();
 const notFound = require('./middeleware/not-found');
